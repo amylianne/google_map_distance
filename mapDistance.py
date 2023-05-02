26b87ae63caf0bcd606df4e66fb7436e13cb9387
amylianne <64841556+amylianne@users.noreply.github.com>
2023-05-02 17:43:57 +0100

    added duration

--- mapDistance.py
+++ mapDistance.py
@@ -3,7 +3,7 @@ import googlemaps
 from datetime import datetime
 
 
-API_KEY = 'api_key'
+API_KEY = ''
 client = googlemaps.Client(API_KEY)
 
 
@@ -16,7 +16,19 @@ def distance_between(start, end, mode):
 
     return distance
 
+def time_taken(start, end, mode):
 
+    # Calculate the distance between the start and end locations at the current time.
+    directions_result = client.directions(start, end, mode, departure_time=datetime.now())
+    # select the first 'leg' of the journey, and assign the distance value to 'distance'
+    duration = directions_result[0]['legs'][0]['duration_in_traffic']['text']
+
+    return duration
+
+# https://maps.googleapis.com/maps/api/distancematrix/json
+#   ?destinations=Greenwich%%
+#   &origins=Stockholm%
+#   &key=
 
 # hard coded values
 # mode is set to driving, but can also use walking, bicycling or transit (public transport)
@@ -29,7 +41,9 @@ end = 'Stockholm , Sweden'
 # end = input("Enter your destination: ")
 
 distance = distance_between(start, end, mode)
+duration = time_taken(start, end, mode)
 
 miles = distance/1609
 
 print(f'The distance between {start} and {end} is {distance} meters or around {round(miles)} miles')
+print('The journey would take '+ duration +'.')
